c74e89736d3767834bdd0f0027c686eb92d33fe0
Osama7amed04 <osssammmah@gmail.com>
2025-11-01 12:19:27 +0200

    Add onboarding screens and update generated files

---
+++ lib/on_boarding/on_boarding_list.dart
@@ -0,0 +1,23 @@
+List<Map<String, String>> on_boarding_list = [
+  {
+    'title': 'Welcome to B-List!',
+    'description':
+        'With our application (B-List), your trip to the store will become more convenient, faster and easier.Make your shopping list in B-List and enjoy!',
+  },
+  {
+    'title': 'Organization',
+    'description':
+        '''Our app allows you to create product categories, mark purchased items and keep track of your budget.
+Everything you need for a convenient and structured shopping experience!''',
+  },
+  {
+    'title': 'Plan and sync',
+    'description':
+        'Plan your shopping without impulse purchases and share the list with your family and friends.',
+  },
+  {
+    'title': 'Free App',
+    'description': '''The Shopping List is offered absolutely free of charge.
+No need to pay for convenience and organization!''',
+  },
+];
